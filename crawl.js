5e5c6cf27c30bc38f68e8e22397ea9edfc7d21ac
Melmow <paulchoi12@naver.com>
2023-10-03 20:10:16 +0100

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,36 @@
+const {JSDOM} = require('jsdom')
+//gives us a way in node to access to the DOM ("Document Model") API 
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    //this is tagging all the <a> </a> elements in the document body
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        // the link Element is an object that includes the HTML body text
+        if(linkElement.href.slice(0, 1)=== '/'){
+            //relative URL
+            try{
+            const urlObj = new URL(`${baseURL}${linkElement.href}`)
+            urls.push(urlObj.href)
+            } catch (err){
+                //the err inside the param is an object that holds
+                //A key called message, that is why we can err.message
+                console.log(`error with relative url: ${err.message}`)
+            }
+        }else {
+            //absolute URL
+            try{
+                const urlObj = new URL(`${linkElement.href}`)
+                urls.push(urlObj.href)
+                } catch (err){
+                    console.log(`error with absolute url: ${err.message}`)
+                }
+        }
+    }
+    return urls
+} 
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     
@@ -13,5 +46,6 @@ function normalizeURL(urlString){
 }
 
 module.exports ={
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require("./crawl")
+const {normalizeURL, getURLsFromHTML} = require("./crawl")
 const {test, expect} = require("@jest/globals")
 
 //take a url and normalize 
@@ -20,4 +20,60 @@ test('normalizeURL strip Http', ()=>{
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
+})
+test('getURLsFromHTML absolute',  ()=>{
+    const inputHTMLBody = `
+    <html> 
+        <body>
+            <a href="https://blog.boot.dev/path/">
+            Boot.dev Blog
+            </a>
+        </body>
+    </html>
+`
+    const inputBaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+    // The purpose of the getURLsFromMTML function is to grab all of urls or links imbedded within an HTML page
+})
+
+test('getURLsFromHTML both relative and absolute',  ()=>{
+    //relative URL are ones without the protocal and domain. its just a path.
+    // the relative here is the same as the URL in the top because
+    // the browser assumes by what domain you are on. if you are in the same
+    // domain, the relative path will be http://<domainYouAreOn>/<relativePath>/
+    const inputHTMLBody = `
+    <html> 
+        <body>
+            <a href="https://blog.boot.dev/path1/">
+            Boot.dev Blog Path One
+            </a>
+            <a href="/path2/">
+            Boot.dev Blog Path two
+            </a>
+        </body>
+    </html>
+`
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+    // The purpose of the getURLsFromMTML function is to grab all of urls or links imbedded within an HTML page
+})
+test('getURLsFromHTML invalid',  ()=>{
+    const inputHTMLBody = `
+    <html> 
+        <body>
+            <a href="invalid">
+                Invalidd URL
+            </a>
+        </body>
+    </html>
+`
+    const inputBaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
+    expect(actual).toEqual(expected)
+    
 })
